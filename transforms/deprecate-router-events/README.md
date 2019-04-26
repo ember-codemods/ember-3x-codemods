5449fa7e287039223144431ed01c5af3ee9fdea9
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-04-26 10:53:29 +0530

    [FEAT] New transform for deprecate-router-events

---
+++ transforms/deprecate-router-events/README.md
@@ -0,0 +1,21 @@
+# deprecate-router-events
+
+
+## Usage
+
+```
+npx ember-3x-codemods deprecate-router-events path/of/files/ or/some**/*glob.js
+
+# or
+
+yarn global add ember-3x-codemods
+ember-3x-codemods deprecate-router-events path/of/files/ or/some**/*glob.js
+```
+
+## Input / Output
+
+<!--FIXTURES_TOC_START-->
+<!--FIXTURES_TOC_END-->
+
+<!--FIXTURES_CONTENT_START-->
+<!--FIXTURES_CONTENT_END-->
---
+++ transforms/deprecate-router-events/__testfixtures__/with-init.input.js
@@ -0,0 +1,25 @@
+import Router from '@ember/routing/router';
+import { inject as service } from '@ember/service';
+
+export default Router.extend({
+  currentUser: service('current-user'),
+
+  init() {
+    this._super(...arguments);
+  },
+
+  willTransition(transition) {
+    this._super(...arguments);
+    if (!this.currentUser.isLoggedIn) {
+      transition.abort();
+      this.transitionTo('login');
+    }
+  },
+
+  didTransition(privateInfos) {
+    this._super(...arguments);
+    ga.send('pageView', {
+      pageName: privateInfos.name
+    });
+  }
+});
