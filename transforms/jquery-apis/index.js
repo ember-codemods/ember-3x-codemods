879dd4389c315fc301ba9f52ca3c13547c9920de
JPetitpierre <98718580+JPetitpierre@users.noreply.github.com>
2023-08-31 14:15:24 +0800

    fix(jquery-apis): update transform to handle jquery method for DOM events

--- transforms/jquery-apis/index.js
+++ transforms/jquery-apis/index.js
@@ -5,32 +5,61 @@ module.exports = function transformer(file, api) {
   const j = getParser(api);
   const options = getOptions();
   const root = j(file.source);
-
   root
-    .find(j.CallExpression, {
-      callee: {
-        object: {
-          callee: {
-            object: {
-              type: "ThisExpression"
-            },
-            property: {
-              name: "$"
-            }
+  .find(j.CallExpression, {
+    callee: {
+      object: {
+        callee: {
+          object: {
+            type: "ThisExpression"
+          },
+          property: {
+            name: "$"
           }
         }
       }
-    })
-    .forEach(path => {
-      //console.log(path);
-    })
-    .replaceWith(path => {
-      //callee.object.arguments
-      //console.log(path.value.callee.object.arguments);
-      const isQuerySelector = path.value.callee.object.arguments.length > 0;
-      //console.log(isQuerySelector);
-      let newCallExp;
-      if (isQuerySelector) {
+    }
+  })
+  .forEach(path => {
+    //console.log(path);
+  })
+  .replaceWith(path => {
+    //callee.object.arguments
+    //console.log(path.value.callee.object.arguments);
+    const isQuerySelector = path.value.callee.object.arguments.length > 0;
+    const isOnOff = ["on","off"].includes(path.value.callee.property?.name);
+    const IsDomEvent = ["click","blur","change","focus","focusin","select","submit","keydown","keypress","keyup","focusout","click","dblclick","focusout","hover","mousedown","mouseenter","mouseleave","mousemove","mouseout","mouseover","mouseup","toggle","error","resize","scroll","load","ready","unload"].includes(path.value.callee.property?.name);
+    //console.log(path.value.callee.property?.name);
+    let newCallExp;
+    if (isQuerySelector) {
+
+      if (isOnOff){
+        const isOn = ["on"].includes(path.value.callee.property?.name);
+
+        newCallExp = j.callExpression(
+          j.memberExpression(
+            j.callExpression(
+              j.memberExpression(
+                j.memberExpression(j.thisExpression(), j.identifier("element"), false),
+                j.identifier("querySelectorAll")
+              ),
+              path.value.callee.object.arguments
+            ),
+            j.identifier("forEach"),
+            false
+          ),
+          [
+            j.arrowFunctionExpression(
+              [j.identifier("el")],
+              j.callExpression(
+                j.memberExpression(j.identifier("el"), j.identifier(isOn? "addEventListener": "removeEventListener"), false),
+                path.value.arguments
+              ),
+              false
+            )
+          ]
+        );
+      }else if (IsDomEvent){
         newCallExp = j.callExpression(
           j.memberExpression(
             j.callExpression(
@@ -48,24 +77,30 @@ module.exports = function transformer(file, api) {
               [j.identifier("el")],
               j.callExpression(
                 j.memberExpression(j.identifier("el"), j.identifier("addEventListener"), false),
-                path.value.arguments
+                [j.literal(path.value.callee.property?.name),...path.value.arguments]
               ),
               false
             )
           ]
         );
-      } else {
-        newCallExp = j.callExpression(
-          j.memberExpression(
-            j.memberExpression(j.thisExpression(), j.identifier("element"), false),
-            j.identifier("addEventListener"),
-            false
-          ),
-          path.value.arguments
-        );
       }
-      return newCallExp;
-    });
+      
+      else{
+        newCallExp = path.node;
+      }
+      
+    } else {
+      newCallExp = j.callExpression(
+        j.memberExpression(
+          j.memberExpression(j.thisExpression(), j.identifier("element"), false),
+          j.identifier("addEventListener"),
+          false
+        ),
+        path.value.arguments
+      );
+    }
+    return newCallExp;
+  });
 
-  return root.toSource();
+return root.toSource();
 }
